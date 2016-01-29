fcf5c69715b9fa2520bc78f44d665cfa68b1c001
Stefan Bjurnemark <stefan.bjurnemark@gmail.com>
2016-01-29 11:36:43 +0100

    Create vertical grid

---
+++ webroot/theme.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Frontcontroller for the grid-theme
+ *
+ */
+
+// Get environment & autoloader and the $app-object.
+require __DIR__.'/config_with_app.php';
+
+// Use clean URL:s
+$app->url->setUrlType(\Anax\Url\CUrl::URL_CLEAN);
+
+// Set the navbar
+$app->navbar->configure(ANAX_APP_PATH . 'config/navbar-grid.php');
+
+// Set the theme
+$app->theme->configure(ANAX_APP_PATH . 'config/theme-grid.php');
+
+// Add routes
+$app->router->add('', function() use ($app) {
+    $app->theme->setVariable('title', "Hello World Pagecontroller")
+           ->setVariable('main', "
+    <h1>Ett nytt tema</h1>
+    <p>Här kommer det nya temat</p>");
+
+});
+
+$app->router->add('regioner', function() use ($app) {
+
+    $app->theme->addStylesheet('css/anax-grid/regions_demo.css');
+    $app->theme->setTitle("Regioner");
+
+    $app->views->addString('flash', 'flash')
+               ->addString('featured-1', 'featured-1')
+               ->addString('featured-2', 'featured-2')
+               ->addString('featured-3', 'featured-3')
+               ->addString('main', 'main')
+               ->addString('sidebar', 'sidebar')
+               ->addString('triptych-1', 'triptych-1')
+               ->addString('triptych-2', 'triptych-2')
+               ->addString('triptych-3', 'triptych-3')
+               ->addString('footer-col-1', 'footer-col-1')
+               ->addString('footer-col-2', 'footer-col-2')
+               ->addString('footer-col-3', 'footer-col-3')
+               ->addString('footer-col-4', 'footer-col-4');
+
+});
+
+$app->router->handle();
+$app->theme->render();
