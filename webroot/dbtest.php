5b22a0b5d57f014647f787cbd456e6abc4fca635
Stefan Bjurnemark <stefan.bjurnemark@gmail.com>
2016-02-03 17:05:21 +0100

    Add CDatabaseModel

--- webroot/dbtest.php
+++ webroot/dbtest.php
@@ -16,30 +16,71 @@ $app->navbar->configure(ANAX_APP_PATH . 'config/navbar_me.php');
 // Set the theme
 $app->theme->configure(ANAX_APP_PATH . 'config/theme_me.php');
 
-// Connect to the database
-$db = new \Mos\Database\CDatabaseBasic();
-$options = require "config_mysql.php";
-$db->setOptions($options);
-$db->connect();
+// Add database as a service
+$di->setShared('db', function() {
+    $db = new \Mos\Database\CDatabaseBasic();
+    $db->setOptions(require ANAX_APP_PATH . 'config/database_mysql.php');
+    $db->connect();
+    return $db;
+});
+
+// Add a controller for users
+$di->set('UsersController', function() use ($di) {
+    $controller = new Anax\Users\UsersController();
+    $controller->setDI($di);
+    return $controller;
+});
+
 
 // Add routes
-$app->router->add('', function() use ($app, $db) {
-    $app->theme->setTitle("Test database");
 
-    $db->select()
-        ->from('Category');
-    $db->execute();
+// Setup database tables
+$app->router->add('setup', function() use ($app) {
 
-    // Get the result
-    $res = $db->fetchAll();
+    $app->db->dropTableIfExists('user')->execute();
 
+    $app->db->createTable(
+        'user',
+        [
+            'id'       => ['integer', 'primary key', 'not null', 'auto_increment'],
+            'acronym'  => ['varchar(20)', 'unique', 'not null'],
+            'email'    => ['varchar(80)'],
+            'name'     => ['varchar(80)'],
+            'password' => ['varchar(255)'],
+            'created'  => ['datetime'],
+            'updated'  => ['datetime'],
+            'deleted'  => ['datetime'],
+            'active'   => ['datetime'],
+        ]
+    )->execute();
 
-    $content = "<pre>" . print_r($res, 1) . "</pre>";
+    $app->db->insert(
+            'user',
+            ['acronym', 'email', 'name', 'password', 'created', 'active']
+        );
 
-    $app->views->add('me/page', [
-        'content' => $content,
-    ]);
+        $now = gmdate('Y-m-d H:i:s');
+
+        $app->db->execute([
+            'admin',
+            'admin@dbwebb.se',
+            'Administrator',
+            password_hash('admin', PASSWORD_DEFAULT),
+            $now,
+            $now
+        ]);
+
+        $app->db->execute([
+            'doe',
+            'doe@dbwebb.se',
+            'John/Jane Doe',
+            password_hash('doe', PASSWORD_DEFAULT),
+            $now,
+            $now
+        ]);
 });
+
+
 
 $app->router->handle();
 $app->theme->render();
